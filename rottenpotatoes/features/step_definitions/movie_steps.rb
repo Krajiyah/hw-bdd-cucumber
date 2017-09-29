28ecc7d5e96f4675efb00b875f05e84c750a4e99
Krishnan Rajiyah <krajiyah@gmail.com>
2017-09-28 22:17:37 -0700

    part 1 done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,12 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  fail "Unimplemented"
+end
+
+And /I am on the RottenPotatoes home page/ do
+  visit root_path
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
